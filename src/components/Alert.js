2cbd831348f67bb58d066cd9faccdcf776050596
CodingDevHub <codingdevhub@gmail.com>
2019-09-22 22:43:29 +0300

    update Alert.js component

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,17 +1,19 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import {AlertContext} from "../context/alert/alertContext";
 
-export const Alert = ({alert}) =>{
+export const Alert = () => {
+    const {alert, hide} = useContext(AlertContext)
 
     // If there is no alert parameter, we will return null and we will not render it
-    if (!alert) {
+    if (!alert.visible) {
         return null
     }
 
     return (
-        <div className="{`alert alert-${alert.type || 'warning'} alert-dismissible`}">
-            <strong>Alert!</strong>
+        <div className={`alert alert-${alert.type || 'warning'} alert-dismissible`}>
+            <strong>Attention!&nbsp;</strong>
             {alert.text}
-            <button type="button" className="close" aria-label="Close">
+            <button onClick={hide} type="button" className="close" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
             </button>
         </div>
