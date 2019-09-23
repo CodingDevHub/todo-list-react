b973409ae814b881fc4026514b27a65ce5d04370
CodingDevHub <codingdevhub@gmail.com>
2019-09-23 23:19:16 +0300

    added loader component

---
+++ src/context/firebase/firebaseReducer.js
@@ -0,0 +1,18 @@
+import {ADD_NOTE, FETCH_NOTES, SHOW_LOADER} from "../types";
+
+const handlers =  {
+    [SHOW_LOADER]: state => ({...state, loading: true}),
+    [ADD_NOTE]: (state, {payload}) => ({
+        ...state,
+        notes: [...state.notes, payload]
+    }),
+
+    [FETCH_NOTES]: (state, {payload}) => ({...state, notes: payload}),
+
+    DEFAULT: state => state
+}
+
+export const firebaseReducer = (state, action) => {
+    const handle = handlers[action.type] || handlers.DEFAULT
+    return handle(state, action)
+}
